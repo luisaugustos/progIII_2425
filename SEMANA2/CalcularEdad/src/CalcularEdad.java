593f51aa6e79913fc6cef02a9c02af1694e1654c
alumno <alumno@gmail.com>
2024-09-23 19:48:54 +0200

    cambio carpetas

---
+++ SEMANA2/CalcularEdad/src/CalcularEdad.java
@@ -0,0 +1,18 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class CalcularEdad {
+    public static void main(String[] args) throws Exception {
+        int anioActual, anioNasc;
+        Scanner scannerRef = new Scanner(System.in);
+        anioActual = LocalDate.now().getYear();
+        System.out.println("Estamos en: "+ anioActual);
+        System.out.println("Escriba su año de nacimiento:");
+        anioNasc = scannerRef.nextInt();
+        int edad = anioActual - anioNasc;
+        System.out.printf("Su edad es: %d años.\n", edad);
+
+        scannerRef.close();
+
+    }
+}
